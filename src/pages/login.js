4b4c5f91f2358b364d4bede5d7e7e568807e5e46
jennifer-pecky <ugwojennifer7@gmail.com>
2023-05-03 12:13:56 +0100

    added art-gallery home page

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div>
+            <div>Hello from Login page</div>
+            <Link href="/">GO home</Link>
+        </div>
+
+    )
+}
