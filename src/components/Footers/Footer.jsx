1c1b7b4cec09ff442ad4bc3f760f2663b3cec2f9
jennifer-pecky <ugwojennifer7@gmail.com>
2023-05-20 14:40:31 +0100

    added foooter section

---
+++ src/components/Footers/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <div className="lg:px-20 bg-black text-[#fff] ">
+      <div className="flex gap-36 items-center p-8 px-20">
+        <h1 className="font-sans text-[18px] font-medium">
+          MODERN <br /> ART GALLERY
+        </h1>
+
+        <p className="font-sans leading-normal text-[13px]">
+          The Modern Art Gallery is free for all visitors and open <br /> seven
+          days a week from 8am to 9am. Find us at 99 <br /> Port Harcourt,
+          Nigeria
+        </p>
+
+        <div className="flex space-x-7">
+          <Link href="/login">
+            <img
+              src="/images/facebook.png"
+              alt="Image"
+              className="w-[20px] bg-white rounded-[10px]"
+            />
+          </Link>
+          <Link href="/login">
+            <img
+              src="/images/instagram.png"
+              alt="Image"
+              className="w-[20px] bg-white rounded-[10px]"
+            />
+          </Link>
+          <Link href="/login">
+            <img
+              src="/images/twitter.png"
+              alt="Image"
+              className="w-[20px] bg-white rounded-[10px]"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Gal-day/Galley.jsx
+++ src/components/Gal-day/Galley.jsx
@@ -1,38 +1,85 @@
 import React from 'react';
 import Image from 'next/image';
+import Footer from '../Footers/Footer';
 
 export default function Galley() {
   return (
-    <div className="lg:px-20 xl:px-20">
-      <div
-        className="lg:grid grid-cols-2 exam
-       p-7"
-      >
+    <>
+      <div className="lg:px-20 ">
         <div
-          className="text-red-400 space-y-5
-        "
+          className="lg:flex gap-14 exam lg:px-20 
+
+       "
         >
-          <h1 className="font-bold lg:text-[4rem] text-[5rem] whitespace-nowrap leading-tight">
-            YOUR DAY AT <br /> THE GALLERY
-          </h1>
-          <p className="font-serif font-medium lg:text-2xl text-[2.3rem] whitespace-nowrap">
-            Wander through our distinct <br /> collection and find new insights{' '}
-            <br /> about our artists. Dive into the <br /> details of their
-            creative process
-          </p>
-        </div>
-        <div className="w-[100%] mere">
-          <Image
-            src="/images/fase.jpg
+          <div
+            className="text-red-400 space-y-5 mt-9
         "
-            alt="Example Image"
-            width={500}
-            height={500}
-            priority="false"
-            className="wave"
-          />
+          >
+            <h1 className="font-bold lg:text-[4rem] text-[4.5rem] whitespace-nowrap leading-tight">
+              YOUR DAY AT <br /> THE GALLERY
+            </h1>
+            <p className="font-serif font-medium lg:text-2xl text-[2.1rem] whitespace-nowrap">
+              Wander through our distinct <br /> collection and find new
+              insights <br /> about our artists. Dive into the <br /> details of
+              their creative process
+            </p>
+          </div>
+          <div className="mere">
+            <Image
+              src="/images/fase.jpg
+        "
+              alt="Example Image"
+              width={700}
+              height={0}
+              priority="false"
+              className="wave"
+            />
+          </div>
+        </div>
+        <br />
+        {/* Second layer */}
+        <div className="flex gap-8 mb-4 px-20">
+          <div className="">
+            <Image
+              src="/images/78.jpg
+        "
+              alt="Example Image"
+              width={500}
+              height={500}
+              priority="false"
+              className=""
+            />
+          </div>
+          <div className="space-y-6">
+            <div className="">
+              <Image
+                src="/images/hero3.jpg
+        "
+                alt="Example Image"
+                width={400}
+                height={500}
+                priority="false"
+                className="wave"
+              />
+            </div>
+
+            <div className="bg-[black] text-[#fff] p-4 w-[100%] h-[45%]">
+              <div className="px-5 mt-4 space-y-3">
+                {' '}
+                <h1 className="text-[2rem] leading-tight font-bold">
+                  COME & BE <br />
+                  INSPIRED
+                </h1>
+                <p className="font-serif">
+                  We`re excited to Welcome you to <br /> our gallery and see how
+                  our <br /> collections infl uence you.{' '}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
